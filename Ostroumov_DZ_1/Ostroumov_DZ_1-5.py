038c523779e82cbfc2c8ed27220319b488ded108
Egor Ostroumov <araimo@yandex.ru>
2021-08-12 11:56:14 +0300

    Решено задание №6

--- Ostroumov_DZ_1/Ostroumov_DZ_1-5.py
+++ Ostroumov_DZ_1/Ostroumov_DZ_1-5.py
@@ -4,8 +4,19 @@
 a = ord(input('Введите первый латинский символ').lower())
 b = ord(input('Введите второй латинский символ').lower())
 
-position_a = a - 97 + 1
-position_b = b - 97 + 1
+if 97 <= a <= 122:
+    if 97 <= b <= 122:
+        position_a = a - 97 + 1
+        position_b = b - 97 + 1
+    else:
+        print('вводите только латинские символы')
+        exit(0)
+else:
+    print('вводите только латинские символы')
+    exit(0)
+
+print(a)
+print(b)
 letters_between = position_a - position_b
 print('позиция символа ', chr(a), ':', position_a)
 print('позиция символа ', chr(b), ':', position_b)
